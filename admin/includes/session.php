8929bfb2a8e1b1b2b2db9bb21b4e05b53dd46a05
ItaMus90 <itamarmuslvi@gmail.com>
2019-08-20 14:44:24 +0300

    gallery_system Session class add check_the_login() method

--- admin/includes/session.php
+++ admin/includes/session.php
@@ -4,12 +4,28 @@
 
     class Session{
 
-        protected $signed_in;
-        public $usre_id;
+        protected $signed_in = false;
+        public $usre_id = null;
 
         public function __construct(){
 
             session_start();
+
+        }
+
+        private function check_the_login(){
+
+            if (isset($_SESSION["user_id"])){
+
+                $this->usre_id = $_SESSION["user_id"];
+                $this->signed_in = true;
+
+            }else{
+
+                unset($this->usre_id);
+                $this->signed_in = false;
+
+            }
 
         }
 
