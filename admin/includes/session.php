dbfac9bdb269c62df8a0ffed077e36bfec9c6942
ItaMus90 <itamarmuslvi@gmail.com>
2019-08-20 14:35:33 +0300

    gallery_system create Session class

--- admin/includes/session.php
+++ admin/includes/session.php
@@ -1,2 +1,19 @@
 <?php
 
+
+
+    class Session{
+
+        protected $signed_in;
+        public $usre_id;
+
+        public function __construct(){
+
+            session_start();
+
+        }
+
+    }
+
+
+    $session = new Session();
