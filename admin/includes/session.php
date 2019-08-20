11bdfb8492ae83a46f38be4d944cc4ec3392924f
ItaMus90 <itamarmuslvi@gmail.com>
2019-08-20 14:52:13 +0300

    gallery_system Session class add login() method

--- admin/includes/session.php
+++ admin/includes/session.php
@@ -10,8 +10,28 @@
         public function __construct(){
 
             session_start();
+            $this->check_the_login();
 
         }
+
+        public function is_signed_in(){
+
+            return $this->signed_in;
+
+        }
+
+        public function login($user){
+
+            if ($user){
+
+                $this->usre_id = $_SESSION["user_id"] = $user->id;
+                $this->signed_in = true;
+
+
+            }
+
+        }
+
 
         private function check_the_login(){
 
