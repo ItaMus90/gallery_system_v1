f8e886ff99c950833721d5aecd65cbded32bb796
ItaMus90 <itamarmuslvi@gmail.com>
2019-08-25 13:40:43 +0300

    gallery_system add properties photo class

--- admin/includes/photo.php
+++ admin/includes/photo.php
@@ -5,6 +5,16 @@
 class Photo extends DB_object{
 
 
+    //static properties
+    protected static $db_table = "photos";
+    protected static $db_table_fields = array('id', 'title', 'description', 'filename', 'type' , 'size');
 
+    //regular properties
+    public $id = null;
+    public $title = null;
+    public $description = null;
+    public $filename = null;
+    public $type = null;
+    public $sze = null;
 
 }
