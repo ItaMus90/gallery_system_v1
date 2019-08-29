a9477a0e1409a0100abcc29ebea4ee1522d0ab9c
ItaMus90 <itamarmuslvi@gmail.com>
2019-08-29 16:43:57 +0300

    gallery_system add delete_user.php

---
+++ admin/delete_user.php
@@ -0,0 +1,28 @@
+<?php include("includes/init.php"); ?>
+
+<?php if (!$session->is_signed_in()){redirect("login.php");} ?>
+
+<?php
+
+    if (empty($_GET["id"]) || !isset($_GET["id"])){
+
+        redirect("users.php");
+
+    }
+
+
+    $user = User::get_by_id($_GET["id"]);
+
+
+    if (key($user)){
+
+        $user->delete();
+        redirect("users.php");
+
+    }else{
+
+        redirect("users.php");
+
+    }
+
+?>
