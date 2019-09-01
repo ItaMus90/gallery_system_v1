ec60df1ec10f149f1106fd1083c203074263a7fd
ItaMus90 <itamarmuslvi@gmail.com>
2019-09-01 16:49:42 +0300

    add delete comment photo page

--- admin/comment_photo.php
+++ admin/comment_photo.php
@@ -63,7 +63,7 @@
                                         <?php echo $comment->author; ?>
                                         <div class="action_links">
 
-                                            <a href="delete_comment.php?id=<?php echo $comment->id; ?>">Delete</a>
+                                            <a href="delete_comment_photo.php?id=<?php echo $comment->id; ?>">Delete</a>
 
                                         </div>
                                     </td>
