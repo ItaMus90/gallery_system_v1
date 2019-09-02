8d08c491507b760111947407d567b5eb61432347
ItaMus90 <itamarmuslvi@gmail.com>
2019-09-02 18:35:43 +0300

    fix issue css and footer

--- includes/footer.php
+++ includes/footer.php
@@ -5,7 +5,7 @@
         <footer>
             <div class="row">
                 <div class="col-lg-12">
-                    <p>Copyright &copy; Your Website 2014</p>
+                    <p class="text-center">Copyright &copy; GSystem <span id="fullyear"></span> </p>
                 </div>
                 <!-- /.col-lg-12 -->
             </div>
@@ -20,6 +20,19 @@
 
     <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
+
+    <script>
+
+        function getFullYear(){
+
+            var year = document.getElementById("fullyear");
+            year.innerText = new Date().getFullYear();
+
+        }
+
+        getFullYear();
+
+    </script>
 
 </body>
 
